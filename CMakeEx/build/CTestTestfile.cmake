eab10f5ed5f02bdd74f806eb2a2b5d599dcc1426
Ppangg <panghyun35@naver.com>
2023-10-19 20:08:45 +0900

    설정 파일 수정/편집

--- CMakeEx/build/CTestTestfile.cmake
+++ CMakeEx/build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(Test1 "/home/team2_2/testGit/RaspberryPi_Check/CMakeEx/build/Exproj")
-set_tests_properties(Test1 PROPERTIES  _BACKTRACE_TRIPLES "/home/team2_2/testGit/RaspberryPi_Check/CMakeEx/CMakeLists.txt;26;add_test;/home/team2_2/testGit/RaspberryPi_Check/CMakeEx/CMakeLists.txt;0;")
+set_tests_properties(Test1 PROPERTIES  _BACKTRACE_TRIPLES "/home/team2_2/testGit/RaspberryPi_Check/CMakeEx/CMakeLists.txt;18;add_test;/home/team2_2/testGit/RaspberryPi_Check/CMakeEx/CMakeLists.txt;0;")
